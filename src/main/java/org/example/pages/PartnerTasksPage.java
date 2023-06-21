2dab337b230371fca81f04fe5d74cf1459aee141
JuliaKosolap <jkosolap@gmail.com>
2023-06-22 00:25:16 +0300

    OptionalTask_CalculateTasksOnFivePages fixed

--- src/main/java/org/example/pages/PartnerTasksPage.java
+++ src/main/java/org/example/pages/PartnerTasksPage.java
@@ -1,10 +1,7 @@
 package org.example.pages;
 
 import com.beust.ah.A;
-import org.openqa.selenium.By;
-import org.openqa.selenium.ElementNotInteractableException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -13,27 +10,41 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PartnerTasksPage extends NavigationMenu{
+public class PartnerTasksPage extends NavigationMenu {
 
     @FindBy(css = "a[aria-label='Next']")
     private WebElement nextPageLink;
+
     public PartnerTasksPage(WebDriver driver) {
         super(driver);
     }
 
     //This method clicks on the Next link in the pagination section until the provided number of page is reached
-    // and returns the names of the Tasks from these pages
+    // and returns the names of the Tasks from these pages.
     public ArrayList<String> getListOfTasksForGivenNumberOfPages(int numberOfPage) throws InterruptedException {
         ArrayList<String> overallTasks = new ArrayList<>();
-        for (int i = 0; i < numberOfPage; i++) {
-            Thread.sleep(30);
-            List<WebElement> tasks = driver.findElements(By.className("col-9"));
-            for (WebElement task: tasks
-            ) {
-                overallTasks.add(task.getText());
+        int index = 1;
+        while (index <= numberOfPage) {
+            List<WebElement> tasks = new WebDriverWait(driver, Duration.ofSeconds(30))
+                    .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className("col-9")));
+            for (int i = 1; i <= tasks.size(); i++) {
+                int attemptCount = 2;
+                int startIndex = 0;
+                while (startIndex < attemptCount) {
+                    try {
+                        String taskName = driver.findElement(By.
+                                xpath("//div[@class='row row-cols-1 row-cols-md-3 searchResultContent']//div[@name='task-card']["
+                                        + i + "]//h5[@class='col-9 font-weight-bold text-left pr-0']")).getText();
+                        overallTasks.add(taskName);
+                        break;
+                    } catch (StaleElementReferenceException e) {
+                        e.printStackTrace();
+                        startIndex++;
+                    }
+                }
             }
             driver.findElement(By.cssSelector("a[aria-label='Next']")).click();
-
+            index++;
         }
         return overallTasks;
     }
@@ -41,7 +52,7 @@ public class PartnerTasksPage extends NavigationMenu{
     public ArrayList<String> getListOfTasksOnCurrentPage() {
         ArrayList<String> tasks = new ArrayList<>();
         List<WebElement> taskCards = new WebDriverWait(driver, Duration.ofSeconds(30))
-                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.name("task-card")));
+                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className("col-9")));
         for (int i = 0; i < taskCards.size(); i++) {
             String task = taskCards.get(i).getText();
             tasks.add(task);
